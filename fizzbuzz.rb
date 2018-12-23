791f2f070e4452c19b687af75ae74d873cd3a973
Kian Edalat <akedalat@gmail.com>
2018-12-23 12:36:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,15 @@
 def fizzbuzz (int)
   if int % 3 == 0
     fizz_3 = "Fizz"
+    return fizz_3
   end
   if int % 5 == 0
   fizz_5 =  "Buzz"
+  return fizz_5
   end
   if int % 3 == 0 && int % 5 == 0
     fizz_15 = "FizzBuzz"
+    return fizz_15
   end
 
 end
