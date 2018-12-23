de4b55cbe9f851c12fdf911c5452f21dadb057f1
Kian Edalat <akedalat@gmail.com>
2018-12-23 12:39:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
+  if (int % 3 == 0 && int % 5 == 0)
+    fizz_15 = "FizzBuzz"
+    return fizz_15
+  end
   if int % 3 == 0
     fizz_3 = "Fizz"
     return fizz_3
@@ -9,9 +13,5 @@ def fizzbuzz (int)
   fizz_5 =  "Buzz"
   return fizz_5
   end
-  if (int % 3 == 0 && int % 5 == 0)
-    fizz_15 = "FizzBuzz"
-    return fizz_15
-  end
-
+  
 end
